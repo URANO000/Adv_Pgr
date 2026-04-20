cabe5fdbd797a1e8ab18fd13af43a24b946493c6
mhlebens <marifermatah@gmail.com>
2026-04-19 20:47:56 -0600

    mvc already done, but i'm havin some troubles with the email

---
+++ AP_MVC/AP_MVC/Models/Adopciones/CrearSolicitudAdopcionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP_MVC.Models.Adopciones
+{
+    public class CrearSolicitudAdopcionViewModel
+    {
+        [Required]
+        public int PublicacionId { get; set; }
+
+        public string UsuarioInteresadoId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debes escribir un mensaje.")]
+        [StringLength(500, ErrorMessage = "El mensaje no puede superar los 500 caracteres.")]
+        public string Mensaje { get; set; } = string.Empty;
+    }
+}
